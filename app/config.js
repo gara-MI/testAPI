da5c188b9f8d74b4796a051af7e1b88aa9342555
Rama <gara@mikrosimage.eu>
2018-06-04 10:51:57 +0200

    don't read from env variable PORT as mongodb also uses this

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
   HOST: process.env.HOST||'localhost',
-  PORT: process.env.PORT||8090,
+  PORT: 3000,
   DB_URI: process.env.MONGO_URL||'mongodb://localhost:27017'
 }
 module.exports = config;
